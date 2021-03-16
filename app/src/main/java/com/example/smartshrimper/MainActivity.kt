78df924200705645b40c09fb5f509aab2b25a1c9
Robert Whitehouse <bob.whitehouse@btinternet.com>
2021-03-16 09:01:30 +0000

    initial BT integration - 09.00 16-3

--- app/src/main/java/com/example/smartshrimper/MainActivity.kt
+++ app/src/main/java/com/example/smartshrimper/MainActivity.kt
@@ -2,6 +2,8 @@ package com.example.smartshrimper
 
 
 import android.bluetooth.BluetoothAdapter
+import android.bluetooth.BluetoothDevice
+import android.bluetooth.BluetoothSocket
 import android.os.Bundle
 import android.view.View
 import android.view.ViewGroup
@@ -14,8 +16,13 @@ import com.google.android.material.floatingactionbutton.FloatingActionButton
 import com.google.android.material.snackbar.Snackbar
 import com.google.android.material.tabs.TabLayout
 import kotlinx.coroutines.delay
+import java.util.*
 
-
+var controllerFound = false
+lateinit var mBTdevice : BluetoothDevice
+lateinit var mBTSocket : BluetoothSocket
+//This is the default well-known UUID
+val mUUID  = UUID.fromString("00001101-0000-1000-8000-00805F9B34FB")
 
 interface SendChannel<in E> {
     suspend fun send(element: E)
@@ -50,28 +57,33 @@ class MainActivity : AppCompatActivity() {
         //Connect to Sensor controller over Bluetooth
 
         val pairedDevices = mBtAdapter.bondedDevices
-
+        val view : View = findViewById(R.id.title)
         if (pairedDevices.size > 0) {
             for (mdevice in pairedDevices) {
                 if (mdevice.name == "Smart Shrimper") {
-                    val view : View = findViewById(R.id.title)
-                    Snackbar.make(view,"Found paired Bluetooth device", Snackbar.LENGTH_LONG)
-                            .setAction("Action", null).show()
+                    Snackbar.make(view,"Found paired Bluetooth device", Snackbar.LENGTH_LONG).show()
+                    mBTdevice = mdevice
+                    controllerFound = true
+                    mBTSocket = mBTdevice.createInsecureRfcommSocketToServiceRecord(mUUID)
+                    //put this in a try/catch block
+                    mBTSocket.connect()
+                }
+                if ( !controllerFound) {
+                    Snackbar.make(view,"No controller found - check Bluetooth settings", Snackbar.LENGTH_LONG).show()
+                    finishAffinity()
                 }
             }
         }
-
     }
 
     override fun onResume() {
         super.onResume()
 
          suspend fun updateGauges() {
+
              while (true) {
- //
-     //
-         //
-             //                revCounter.moveToValue(streamBuffer.readNext().toFloat())
+
+             //  revCounter.moveToValue(streamBuffer.readNext().toFloat())
                  delay(200L)
              }
          }
